d7e8a99e1a5bac9188fb4ccd69460545b87c3f7b
Chaeoh <limchyo>
2018-06-03 16:49:03 +0900

    Add 02_data_type-1.py

---
+++ 01_conversation.py
@@ -0,0 +1,20 @@
+# 화면에 데이터 값 출력하기
+print(1+2)
+
+# input을 활용하여 값 받기
+# 변수 활용
+'''
+name = input("당신의 이름은 무엇입니까? ")
+print("안녕하세요, 제 이름은 " + name + "입니다.")
+
+
+division = input("당신의 소속은 어디입니까? ")
+print("제 소속은 " + division + "입니다.")
+
+
+career = input("당신의 직업은 무엇입니까? ")
+print("제 직업은 "+ career +"입니다.")
+'''
+
+Who = input("넌 누구니? ")
+print("난 "+ Who +"다.")
---
+++ 02_data_type-1.py
@@ -0,0 +1,19 @@
+#정수 int - integer
+print(1+1)
+# 소수 float
+print(1.5+2.1)
+# 문자 str - string
+print("hello")
+
+# 문자로서 문자와 숫자 더하기
+print("hello" + "1")
+
+# 정수 소수 간 변경
+print(4 // 2)
+print(int(4/2))
+print(float(2))
+
+# 문자열 숫자 간 변경
+print("1")
+print(int("1"))
+print(int("1.5")) # 에러 발생. 1.5는 정수가 아니므로.
