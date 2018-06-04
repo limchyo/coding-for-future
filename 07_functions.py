e3faea7cfc29678ff1766edcd0830c589d09028f
Chaeoh <limchyo>
2018-06-04 15:14:50 +0900

    functions practice

---
+++ 07_functions.py
@@ -0,0 +1,26 @@
+def hello_friends(name):
+    print("hey, {}".format(name))
+
+name1 = "cheo"
+name2 = "john"
+name3 = "ryan"
+name4 = "philip"
+name5 = "michael"
+
+hello_friends(name1)
+hello_friends(name2)
+hello_friends(name3)
+hello_friends(name4)
+hello_friends(name5)
+
+def sum(a, b):
+
+    return a + b
+
+print(sum(1, 2))
+print(sum(12511, 21242))
+
+def cheo_def(a, b, c):
+    return a + b + c, a * b * c
+
+print(cheo_def(1,2,3))
