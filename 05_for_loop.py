e0f26c2e462b9c4fba70181a5036c63bf00a69c4
Chaeoh <limchyo>
2018-06-04 12:55:18 +0900

    add more

--- 05_for_loop.py
+++ 05_for_loop.py
@@ -1,9 +1,19 @@
-for num in range(1, 10):
+for num in range(1, 7):
     print(num)
+
+for count in range(7):
+    print(count)
+
+for count in range(4):
+    print('횟수:', count)
 
 num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for num in num_list:
     print(num)
+
+my_list = [1, 2, 3, 4]
+for count in my_list :
+    print("횟수:",count)
 
 a = 1
 while a > 10:
