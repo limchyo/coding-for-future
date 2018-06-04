d0c6f86111d809a4cd97443b737074e349babee4
Chaeoh <limchyo>
2018-06-04 15:51:54 +0900

    add random_choice.py

--- random_choice.py
+++ random_choice.py
@@ -1,5 +1,13 @@
+# 랜덤 뽑기
 import random
-
 random_list = [1,2,3,4,5]
 
 print(random.choice(random_list))
+print(random.randint(1,6))
+
+# 인자 길이 줄여서 출력하기
+lotto = random.choice(random_list)
+print(lotto)
+
+# 여러 개 동시에 뽑기
+print(random.sample(random_list,3))
