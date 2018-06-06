e0e3878796127aef44a37c761a1384b060db7109
Chaeoh <limchyo>
2018-06-06 09:13:47 +0900

    1st assignment 'menu choice'

---
+++ menu_choice.py
@@ -0,0 +1,14 @@
+import random
+
+menu = input("한식, 중식, 일식 중 어떤 걸 드시겠습니까? ")
+
+한식 = ['계절밥상', '자연별곡', '풀잎채']
+중식 = ['홍콩반점', '공화춘', '딘타이펑']
+일식 = ['요시노야', '야요이켄', '코코이치방야']
+
+if menu == '한식':
+    print(random.choice(한식))
+elif menu == '중식':
+    print(random.choice(중식))
+elif menu == '일식':
+    print(random.choice(일식))
