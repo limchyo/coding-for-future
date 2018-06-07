70df1b5601c0478931b2c202daee9f6bd25b7d60
Chaeoh <limchyo>
2018-06-07 16:27:39 +0900

    self-study with For_statement

---
+++ For_statement_practice.py
@@ -0,0 +1,33 @@
+
+
+# 00100
+# 01110
+# 11111
+# 01110
+# 00100
+
+# for num in range(1, 6):
+#     print("1" * num)
+
+# for a in range(1, 101):
+#     print(a)
+
+
+# A = [70, 60, 55, 75, 95, 90, 80, 80, 85, 100] #평균점수(for문)
+# b = 0 # 0으로 시작해서 A의 값을 하나씩 받을 준비.
+# for a in A: # 70부터 하나씩 값이 출력
+#     b += a # b는 a의 값이 순차적으로 더해진다.
+# average = b / len(A) # 총 더해진 b의 값에 A의 길이로 나눈다.
+# print(average)
+
+
+# result = 0
+# for i in range(1, 1001):
+#      if i % 5 == 0:
+#          result += i #(0에서 5씩 계속 반복적으로 더 해준다)
+# print(result) #(따라서 5씩 더해진 것, 즉 총합이 구해진다)
+
+Blood = ['A', 'B', 'A', 'O', 'AB', 'AB', 'O', 'A', 'B', 'O', 'B', 'AB']
+
+for A in Blood:
+    for A == 'A':
