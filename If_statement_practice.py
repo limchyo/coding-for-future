1b456e6cd3a2b9b3d5884eae63021acc8183591e
Chaeoh <limchyo>
2018-06-08 15:34:24 +0900

    If_statement_practice.py

---
+++ If_statement_practice.py
@@ -0,0 +1,40 @@
+# card = 0
+# money = 5000
+# if money >= 4000 or card:
+#     print("택시를 탈 수 있다")
+# else:
+#     print("택시를 탈 수 없다")
+
+# bag = ['bottle', 'sports wear', 'shoes']
+# if 'shoes' in bag:
+#     print("I am ready!")
+# else:
+#     print("넌 운동할 준비가 안되있다")
+
+# people = ['Korean', 'Japanese', 'American']
+# if 'Japanese' in people:
+#     pass
+# else:
+#     print("별도의 입국심사가 필요합니다")
+
+# lucky_list = [1, 9, 23, 46]
+# win = 1
+# num = int(input("숫자 1, 3, 9, 20, 23, 40, 46 중 하나를 고르시오 "))
+#
+# if num in lucky_list and win:
+#     print("야호")
+# else:
+#     print("실패")
+
+
+# num = int(input("아무 숫자나 입력하시오. "))
+#
+# if num % 2 == 0: print("짝수입니다")
+# else: print("홀수입니다")
+
+# age = 30
+# height = 180
+# if age < 30 and height >= 175:
+#     print("YES")
+# else:
+#     print("NO")
